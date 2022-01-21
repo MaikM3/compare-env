935c885f6385e0a8f3d2ba31b7547fd6991934b1
Harry Turnbull <hturnbull@live.co.uk>
2022-01-21 11:29:31 +0000

    Refactor getFiles to use synchronous functions

--- utils/getFiles.js
+++ utils/getFiles.js
@@ -1,23 +1,19 @@
-const { promisify } = require('util');
-const { resolve } = require('path');
-const fs = require('fs');
-
-const readdir = promisify(fs.readdir);
-const stat = promisify(fs.stat);
+const { resolve } = require("path");
+const { readdirSync, statSync } = require("fs");
 
 /**
  * Recursively finds all files in a directory
- * 
+ *
  * @param {string} dir - The directory to start searching from
  * @returns {Array} Array of filepaths
  */
-async function getFiles(dir) {
-  const subdirs = await readdir(dir);
-  const files = await Promise.all(subdirs.map(async (subdir) => {
-    const res = resolve(dir, subdir);
-    return (await stat(res)).isDirectory() ? getFiles(res) : res;
-  }));
-  return files.reduce((a, f) => a.concat(f), []);
+function getFiles(dir) {
+  const subdirs = readdirSync(dir);
+  const files = subdirs.map((subdir) => {
+    const path = resolve(dir, subdir);
+    return (statSync(path)).isDirectory() ? getFiles(path) : path;
+  });
+  return files.flat();
 }
 
-module.exports = getFiles
+module.exports = getFiles;
