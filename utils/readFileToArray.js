ce8bacd1e487618057a6d94d3c222bad9479b172
Harry Turnbull <hturnbull@live.co.uk>
2022-01-21 15:50:02 +0000

    Handle invalid paths in readFileToArray

--- utils/readFileToArray.js
+++ utils/readFileToArray.js
@@ -1,8 +1,19 @@
+const chalk = require("chalk");
 const fs = require("fs");
+const { promisify } = require("util");
+const readFile = promisify(fs.readFile);
 
-const readFileToArray = (filePath) => {
-  const file = fs.readFileSync(filePath).toString();
-  return file.split("\n");
+const readFileToArray = async (filePath) => {
+  const buffer = await readFile(filePath).catch((error) => {
+    if (error.message.includes("ENOENT: no such file or directory")) {
+      console.log(
+        chalk.blue.bold("The selected .env file is no longer present")
+      );
+      console.log(`Path: ${chalk.green(filePath)}`);
+      return;
+    } else throw error;
+  });
+  return buffer && buffer.toString().split("\n");
 };
 
 exports.readFileToArray = readFileToArray;
