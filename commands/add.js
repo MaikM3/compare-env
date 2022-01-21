7b269c24dba3e6255cdafb12d26cfa84aa9ea5e7
Harry Turnbull <hturnbull@live.co.uk>
2022-01-21 13:23:15 +0000

    Add checks store for existing env file paths and disables them in the list

--- commands/add.js
+++ commands/add.js
@@ -1,28 +1,31 @@
 const inquirer = require("inquirer");
 const getFiles = require("../utils/getFiles");
 
-const conf = new (require('conf'))()
+const conf = new (require("conf"))();
 
-const ENV_FILES = 'compare-envs.envs.files'
+const ENV_FILES = "compareEnvs.envs.files";
 
 const add = async () => {
   const cwd = process.cwd();
   const cwdFilePrefix = `${cwd}/`;
 
+  const storedEnvs = conf.get(ENV_FILES) || [];
+
   const files = getFiles(cwd);
   const envFiles = files.filter((file) => file.includes(".env"));
-  const envFileEntries = envFiles.map((file) => [
-    file.replace(cwdFilePrefix, ""),
-    file,
-  ]);
-  const envFileMap = Object.fromEntries(envFileEntries);
+
+  const choices = envFiles.map((file) => ({
+    name: file.replace(cwdFilePrefix, ""),
+    value: file,
+    disabled: storedEnvs.includes(file) && "Already added",
+  }));
 
   const { chosenEnvs } = await inquirer
     .prompt({
       name: "chosenEnvs",
       message: "Choose any number of .env files to add\n",
       type: "checkbox",
-      choices: Object.keys(envFileMap),
+      choices,
     })
     .catch((error) => {
       if (error.isTtyError) {
@@ -31,14 +34,11 @@ const add = async () => {
       console.error(error);
     });
 
-  const storedEnvs = conf.get(ENV_FILES)
-  const envPathsToStore = chosenEnvs.map(chosenEnv => envFileMap[chosenEnv])
+  const uniqueEnvPaths = [...new Set([...storedEnvs, ...chosenEnvs])];
+  conf.set(ENV_FILES, uniqueEnvPaths);
 
-  const uniqueEnvPaths = [...new Set([...(storedEnvs ? storedEnvs : []), ...envPathsToStore])]
-  conf.set(ENV_FILES, uniqueEnvPaths)
-
-  const check = conf.get(ENV_FILES)
-  console.log('check :>> ', check);
+  const check = conf.get(ENV_FILES);
+  console.log("check :>> ", check);
 };
 
 module.exports = add;
