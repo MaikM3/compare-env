a3dd5d80da3b8c265182a9db361c858cab54e492
Harry Turnbull <hturnbull@live.co.uk>
2022-01-21 11:53:48 +0000

    Add finds all .envs within cwd and offers choice of which .envs to choose

--- commands/add.js
+++ commands/add.js
@@ -1,5 +1,33 @@
-const add = () => {
-  console.log('Add command')
-}
+const inquirer = require("inquirer");
+const getFiles = require("../utils/getFiles");
 
-module.exports = add
+const add = async () => {
+  const cwd = process.cwd();
+  const cwdFilePrefix = `${cwd}/`;
+
+  const files = getFiles(cwd);
+  const envFiles = files.filter((file) => file.includes(".env"));
+  const envFileEntries = envFiles.map((file) => [
+    file.replace(cwdFilePrefix, ""),
+    file,
+  ]);
+  const envFileMap = Object.fromEntries(envFileEntries);
+
+  const choice = await inquirer
+    .prompt({
+      name: "envs",
+      message: "Choose any number of .env files to add\n",
+      type: "checkbox",
+      choices: Object.keys(envFileMap),
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        console.error("Prompt couldn't be rendered in the current environment");
+      }
+      console.error(error);
+    });
+
+  console.log("choice :>> ", choice);
+};
+
+module.exports = add;
