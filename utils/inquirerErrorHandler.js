eed8d0bdce5784ef9301d2bdc9766be5656bce3f
Harry Turnbull <hturnbull@live.co.uk>
2022-01-21 14:50:06 +0000

    Add JSDoc to inquirerErrorHandler

--- utils/inquirerErrorHandler.js
+++ utils/inquirerErrorHandler.js
@@ -1,3 +1,8 @@
+/**
+ * Basic error handling for inquirer prompts
+ * 
+ * @param {error} error - The inquirer error
+ */
 const inquirerErrorHandler = (error) => {
   if (error.isTtyError) {
     console.error("Prompt couldn't be rendered in the current environment");
