19a6671662c6c236bedb2a34cb3fb0208188f0f1
Harry Turnbull <hturnbull@live.co.uk>
2022-01-21 15:11:21 +0000

    Add compareArrays util

---
+++ utils/compareArrays.js
@@ -0,0 +1,12 @@
+/**
+ * Predicte that compares two arrays containing strings/numbers. Will not handle
+ * deep object or array comparison.
+ * 
+ * @param {Array} arr1 - The first array
+ * @param {Array} arr2 - The second array
+ * @returns {boolean} If the arrays contains the same elements
+ */
+const compareArrays = (arr1, arr2) =>
+  arr1.length === arr2.length && JSON.stringify(arr1.sort()) === JSON.stringify(arr2.sort());
+
+exports.compareArrays = compareArrays
