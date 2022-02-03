0e2f8d6f0bf8f8620693f986a3983c19011fdeb1
Harry Turnbull <hturnbull@live.co.uk>
2022-02-03 16:06:25 +0000

    Add comparePaths util

---
+++ utils/comparePaths.js
@@ -0,0 +1,39 @@
+const path = require('path')
+/**
+ * Prepares any number of envPaths and replaces their common parts with "..", relative to the current working directory.
+ *
+ * @param  {Array} envPaths
+ * @returns
+ */
+const comparePaths = (envPaths) => {
+  const cwd = process.cwd()
+
+  const relativeEnvPaths = envPaths.map(envPath => path.relative(cwd, envPath))
+
+  const splitenvPaths = relativeEnvPaths.map((envPath) => envPath.split("/").filter(Boolean));
+
+  const longestPathIndex = splitenvPaths.reduce(
+    (longestIndex, splitPath, index) =>
+      splitPath.length > splitenvPaths[longestIndex].length ? index : longestIndex,
+    0
+  );
+  const longestPath = splitenvPaths[longestPathIndex]
+
+  let replacedEnvPaths = splitenvPaths.map(() => [])
+
+  longestPath.forEach((_segment, index) => {
+    const correspondingSegments = splitenvPaths.map(splitPath => splitPath[index])
+
+    const allSegmentsTheSame = new Set(correspondingSegments).size === 1
+
+    if (allSegmentsTheSame) {
+      replacedEnvPaths.forEach(envPath => envPath.push('..'))
+    } else {
+      replacedEnvPaths.forEach((envPath, index) => envPath.push(correspondingSegments[index]))
+    }
+  })
+
+  return replacedEnvPaths.map(replacedPath => replacedPath.join('/'));
+};
+
+exports.comparePaths = comparePaths;
