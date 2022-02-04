393605b02462b8ce0cb24e880603a17907f91bff
Harry Turnbull <hturnbull@live.co.uk>
2022-02-04 11:13:12 +0000

    extract createSection helper from compareEnvFiles

--- utils/compareEnvFiles.js
+++ utils/compareEnvFiles.js
@@ -36,9 +36,10 @@ const compareEnvFiles = (env1, env2) => {
     ([_, value]) => !value.content
   );
 
-  table.push(createHeader("Shared keys with same values"));
-  table.push(...sharedKeyValueFilledCells);
-  table.push(...sharedKeyValueEmptyCells);
+  table.push(...createSection("Shared keys with same values", [
+    ...sharedKeyValueFilledCells,
+    ...sharedKeyValueEmptyCells,
+  ]));
 
   const sharedKeyCells = transformedFile1
     .filter(([key1, value1]) =>
@@ -52,32 +53,36 @@ const compareEnvFiles = (env1, env2) => {
       transformedFile2.find(([key2, value]) => key1 === key2)[1],
     ]);
 
-  table.push(createHeader("Shared keys with different values"));
-  table.push(...sharedKeyCells);
+  table.push(...createSection("Shared keys with different values", sharedKeyCells));
 
   const uniqueKeysInEnv1 = transformedFile1.filter(
     ([key1]) => !transformedFile2.find(([key2]) => key1 === key2)
   );
 
-  table.push(createHeader(`Unique keys in ${env1.name}`));
-  table.push(...uniqueKeysInEnv1);
+  table.push(...createSection(`Unique keys in ${env1.name}`, uniqueKeysInEnv1));
 
-  const uniqueKeysInEnv2 = transformedFile2.filter(
-    ([key2]) => !transformedFile1.find(([key1]) => key2 === key1)
-  ).map(([key, value]) => [key, "", value]);
+  const uniqueKeysInEnv2 = transformedFile2
+    .filter(([key2]) => !transformedFile1.find(([key1]) => key2 === key1))
+    .map(([key, value]) => [key, "", value]);
 
-  table.push(createHeader(`Unique keys in ${env2.name}`));
-  table.push(...uniqueKeysInEnv2);
+  table.push(...createSection(`Unique keys in ${env2.name}`, uniqueKeysInEnv2));
 
   console.log(table.toString());
 };
 
 exports.compareEnvFiles = compareEnvFiles;
 
-const createHeader = (text) => [
-  {
-    colSpan: 3,
-    content: chalk.green.bold(text),
-    hAlign: "center",
-  },
-];
+const createSection = (heading, keys) => {
+  const noKeys = keys.length === 0;
+  const headingText = noKeys
+    ? chalk.red.bold(`No ${heading[0].toLowerCase() + heading.slice(1)}`)
+    : chalk.green.bold(heading);
+  const headingCell = [
+    {
+      colSpan: 3,
+      content: headingText,
+      hAlign: "center",
+    },
+  ];
+  return [headingCell, ...keys];
+};
