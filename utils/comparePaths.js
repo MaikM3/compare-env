f67e28c3a620d960667169a4c25d93bcb0199b85
Harry Turnbull <hturnbull@live.co.uk>
2022-02-04 10:12:35 +0000

    Fix bug in comparePaths where shorter paths were having items added beyond their last item

--- utils/comparePaths.js
+++ utils/comparePaths.js
@@ -1,4 +1,4 @@
-const path = require('path')
+const path = require("path");
 /**
  * Prepares any number of envPaths and replaces their common parts with "..", relative to the current working directory.
  *
@@ -6,34 +6,45 @@ const path = require('path')
  * @returns
  */
 const comparePaths = (envPaths) => {
-  const cwd = process.cwd()
+  const cwd = process.cwd();
 
-  const relativeEnvPaths = envPaths.map(envPath => path.relative(cwd, envPath))
+  const relativeEnvPaths = envPaths.map((envPath) =>
+    path.relative(cwd, envPath)
+  );
 
-  const splitenvPaths = relativeEnvPaths.map((envPath) => envPath.split("/").filter(Boolean));
+  const splitenvPaths = relativeEnvPaths.map((envPath) =>
+    envPath.split("/").filter(Boolean)
+  );
 
   const longestPathIndex = splitenvPaths.reduce(
     (longestIndex, splitPath, index) =>
-      splitPath.length > splitenvPaths[longestIndex].length ? index : longestIndex,
+      splitPath.length > splitenvPaths[longestIndex].length
+        ? index
+        : longestIndex,
     0
   );
-  const longestPath = splitenvPaths[longestPathIndex]
+  const longestPath = splitenvPaths[longestPathIndex];
 
-  let replacedEnvPaths = splitenvPaths.map(() => [])
+  let replacedEnvPaths = splitenvPaths.map(() => []);
 
   longestPath.forEach((_segment, index) => {
-    const correspondingSegments = splitenvPaths.map(splitPath => splitPath[index])
+    const correspondingSegments = splitenvPaths.map(
+      (splitPath) => splitPath[index]
+    );
 
-    const allSegmentsTheSame = new Set(correspondingSegments).size === 1
+    const allSegmentsTheSame = new Set(correspondingSegments).size === 1;
 
     if (allSegmentsTheSame) {
-      replacedEnvPaths.forEach(envPath => envPath.push('..'))
+      replacedEnvPaths.forEach((envPath) => envPath.push(".."));
     } else {
-      replacedEnvPaths.forEach((envPath, index) => envPath.push(correspondingSegments[index]))
+      replacedEnvPaths.forEach((envPath, index) => {
+        const correspondingSegment = correspondingSegments[index];
+        if (correspondingSegment) envPath.push(correspondingSegment);
+      });
     }
-  })
+  });
 
-  return replacedEnvPaths.map(replacedPath => replacedPath.join('/'));
+  return replacedEnvPaths.map((replacedPath) => replacedPath.join("/"));
 };
 
 exports.comparePaths = comparePaths;
