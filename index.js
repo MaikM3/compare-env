8b14abaf21de3e98a435dcc6cc2a3416c10870dd
Harry Turnbull <hturnbull@live.co.uk>
2022-02-04 12:17:11 +0000

    Add version and description to cli help

--- index.js
+++ index.js
@@ -3,6 +3,12 @@
 const { program } = require("commander");
 const { add } = require("./commands/add");
 const { compare } = require("./commands/compare");
+const packageJson = require("./package.json");
+
+program
+  .name("compare-env")
+  .description(packageJson.description)
+  .version(packageJson.version);
 
 // Add a path to an .env file for later comparison. Will either take a
 // path, or search from current directory to find .env files
